1618127587ebe8a93d1c91a6e1c21b4535a625e7
CarlosCente <31189121+CarlosCente@users.noreply.github.com>
2020-09-30 14:55:05 +0200

    Correcion primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { MaterialModule } from './material.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
